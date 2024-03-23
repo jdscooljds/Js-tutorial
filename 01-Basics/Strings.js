a4d90c937002f2c8a53da8a19b343f592f837924
jdscooljds <67825593+jdscooljds@users.noreply.github.com>
2024-03-23 05:10:42 +0000

    Pending changes exported from your codespace

---
+++ 01-Basics/Strings.js
@@ -0,0 +1,28 @@
+const name = "Joydeep"
+const repoCount = 50
+
+console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Joydeep-js-com')
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('p'));
+/*const newString = gameName.substring(0, 4)
+console.log(newString);*/
+
+/*const newgame= gameName.slice(-9,6)
+console.log(newgame);*/
+
+/*const newStringone = '    hare krishna    '
+console.log(newStringone);
+console.log(newStringone.trim());*/
+
+// const url = "https://hitesh.com/hitesh%20choudhary"
+
+//console.log(url.replace('%20', '-'))
+
+//console.log(url.includes('sundar'))
+
+//console.log(gameName.split('-'));
